eb28cc513d7c3bd26f8710d2018a7379d197be1c
Chloe <11739209+chloecy@users.noreply.github.com>
2020-08-24 10:19:31 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+const randomImageSource = "./images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+const randomImageSource2 = "./images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+const refreshButton = document.querySelector('.refresh-button');
+
+const refreshPage = () => {
+  window.location.reload();
+}
+
+refreshButton.addEventListener('click', refreshPage);
+
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerHTML = "🎉 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+	document.querySelector("h1").innerHTML = "Player 2 Wins! 🎉"
+}
+else {
+	document.querySelector("h1").innerHTML = "Draw";
+}
+
